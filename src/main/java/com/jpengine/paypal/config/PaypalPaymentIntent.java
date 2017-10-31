088402c325f3dadde690bd05f548177ae80d7f91
ttirumala <ttirumala@gmail.com>
2017-10-31 18:56:25 +0530

    Initial commit

---
+++ src/main/java/com/jpengine/paypal/config/PaypalPaymentIntent.java
@@ -0,0 +1,7 @@
+package com.jpengine.paypal.config;
+
+public enum PaypalPaymentIntent {
+
+	sale, authorize, order
+
+}
---
+++ src/main/java/com/jpengine/paypal/config/PaypalPaymentMethod.java
@@ -0,0 +1,7 @@
+package com.jpengine.paypal.config;
+
+public enum PaypalPaymentMethod {
+
+	credit_card, paypal
+
+}
